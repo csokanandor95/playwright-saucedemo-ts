d11d0e56794fc9d7113ad4e158f5dca42cefa904
csokanandor95 <csokanandor77@gmail.com>
2026-07-16 09:36:37 +0200

    Create checkout.ts test-data file for checkout tests

---
+++ test-data/checkouts.ts
@@ -0,0 +1,15 @@
+export const PRODUCT_CART = {
+  productForCart:  'Sauce Labs Backpack',
+};
+
+export const CHECKOUT_DETAILS = {
+  firstName:  'John',
+  lastName:   'Doe',
+  postalCode: '12345',
+};
+
+export const ERROR_MESSAGES = {
+  emptyFirstName: 'Error: First Name is required',
+  emptyLastName:  'Error: Last Name is required',
+  emptyPostalCode: 'Error: Postal Code is required',
+};
